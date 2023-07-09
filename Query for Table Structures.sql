df545de1730309b4cbab81e71ccc3ed9fd291254
ilham-muhammad <mg.muhammad.ilham@gmail.com>
2023-07-09 11:53:24 +0800

    editing

--- Query for Table Structures.sql
+++ Query for Table Structures.sql
@@ -8,8 +8,8 @@ CREATE TABLE City(
 	city_id int UNIQUE PRIMARY KEY NOT NUll,
 	city varchar(50) NOT NULL,
 	province varchar(50),
-	postal_code int,
-	location point
+	latitude float(24),
+	longitude float(24)
 )
 
 -- Creating table : Cars
